3f096de18d914ebbd602e0d7e12ff3b21daa32aa
Rimon <rimonyousuf00@gmail.com>
2024-07-06 22:49:48 +0600

    Updated all things

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,9 +1,11 @@
+import BkashPayment from "./BkashPayment";
 import CardPayment from "./CardPayment";
 
 const CheckoutForm = () => {
     return (
         <div>
             <CardPayment/>
+            <BkashPayment/>
         </div>
     );
 };
